dd5c09608d6420b5fbf69567351c21b7fd1ae4c3
timalces <tim.brackfield@alces-software.com>
2022-02-10 15:06:27 +0000

    styling and validation improvements

--- app/javascript/packs/create_event.js
+++ app/javascript/packs/create_event.js
@@ -2,6 +2,9 @@ window.addEventListener('DOMContentLoaded', (event) => {
   $('.tool-tip').tooltip();
   $('.simple-node-count').change(validateCounts);
   $('.when-radio').change(toggleDateSelectors);
+  $('.day-input').change(updateWeekdays);
+  $('.when-radio').change(validateTimings);
+  $('.scheduled-input').change(validateTimings);
   $('#wizard-next-button').click(showNextSection);
   $('#wizard-back-button').click(showPreviousSection);
 });
@@ -50,8 +53,72 @@ function validateCounts(){
   if(!changes) {
     nextButton.addClass('disabled');
     nextButton.prop('disabled', true);
+    nextButton.prop('title', "No counts selected");
   } else {
     nextButton.removeClass('disabled');
     nextButton.prop('disabled', false);
+    nextButton.prop('title', "");
+  }
+}
+
+function validateTimings() {
+  let nextButton = $('#wizard-next-button');
+  let valid = false;
+  if ($('#time-now').prop("checked")) {
+    valid = true;
+  } else {
+    let time = $('#scheduled-time').val();
+    let date = $('#scheduled-date').val();
+    let weekdays = $('#weekdays').val();
+    let endDate = $('#end-date').val();
+    if (time === "" || date === "") {
+      nextButton.prop("disabled", true);
+      nextButton.prop("title", "Date and time must be specified");
+      nextButton.addClass("disabled");
+    } else if (weekdays != "" && endDate === "" || weekdays === "" && endDate != "") {
+      nextButton.prop("disabled", true);
+      nextButton.prop("title", "For repeated policy, weekdays and an end date must be selected");
+      nextButton.addClass("disabled");
+    } else if (endDate != "" && new Date(endDate) < new Date(date)) {
+      nextButton.prop("disabled", true);
+      nextButton.prop("title", "For repeated policy, end date must be after or equal to start date");
+      nextButton.addClass("disabled");
+    } else {
+      let oneHourAhead = new Date();
+      oneHourAhead.setHours(oneHourAhead.getHours() + 1);
+      let fullDate = new Date(`${date} ${time}`);
+      if (weekdays === "" && fullDate < oneHourAhead) {
+        submitButton.prop("disabled", true);
+        submitButton.prop("title", "Must be at least one hour in the future");
+        submitButton.addClass("disabled");
+      } else {
+        valid = true;
+      }
+    }
+  }
+  if (valid) {
+    nextButton.removeClass('disabled');
+    nextButton.prop('disabled', false);
+    nextButton.prop('title', '');
+  }
+}
+
+function updateWeekdays() {
+  let any = false;
+  let weekdays = [];
+  $('.day-input').each(function() {
+    if($(this).prop('checked')) {
+      weekdays.push("1");
+      any = true;
+    } else {
+      weekdays.push("0");
+    }
+  });
+  if(any) {
+    $('#weekdays').val(weekdays.join(""));
+    $('#type').val("RepeatedScheduledRequest");
+  } else {
+    $('#weekdays').val(null);
+    $('#type').val("OneOffScheduledRequest");
   }
 }
