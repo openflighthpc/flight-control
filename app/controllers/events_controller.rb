a4d673aef7907b41376a77235485eb1a191a8b78
timalces <tim.brackfield@alces-software.com>
2022-02-11 10:59:39 +0000

    updated form logic

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,6 +7,28 @@ class EventsController < ApplicationController
 
   def create
     get_project
-    render json: params.to_json
+    render json: filtered_change_request_params.to_json
+  end
+
+  def filtered_change_request_params
+    permitted = params.permit(
+      :counts_criteria,
+      :timeframe,
+      :date,
+      :time,
+      :weekdays,
+      :type,
+      :end_date,
+      :description,
+      nodes: {}
+    )
+    filtered = permitted.transform_values do |value|
+      # filter hash within params
+      if value.class == ActionController::Parameters
+        value.select { |k, v| v != "" }
+      else
+        value == "" ? nil : value
+      end
+    end
   end
 end
