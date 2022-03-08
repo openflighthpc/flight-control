6792be9e053a1bced0fba6162dc1f0bee27d6746
VoxSecundus <millard64@hotmail.co.uk>
2022-03-08 10:51:11 +0000

    Format flash messages properly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,13 @@ module ApplicationHelper
     rgb.map! {|f| [(f + 255 * amount).round, 255].min }
     "#%02x%02x%02x" % rgb
   end
+
+  def flash_class(key)
+    case key
+    when "notice" then "alert alert-info"
+    when "success" then "alert alert-success"
+    when "error" then "alert alert-danger"
+    when "alert" then "alert alert-warning"
+    end
+  end
 end
