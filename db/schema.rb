953e6c4f7835cfb60bb229db1113f5ab49513814
VoxSecundus <millard64@hotmail.co.uk>
2022-04-01 17:19:40 +0100

    Reorder migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_23_163905) do
+ActiveRecord::Schema.define(version: 2022_04_01_161839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
