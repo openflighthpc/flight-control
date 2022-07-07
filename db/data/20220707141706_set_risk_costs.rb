a37f0e331d4a7dda1ee15d57fd01505eb3fc358b
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2022-07-07 15:45:17 +0100

    Fix data migration and reset dev environment config

--- db/data/20220707141706_set_risk_costs.rb
+++ db/data/20220707141706_set_risk_costs.rb
@@ -2,7 +2,7 @@ class SetRiskCosts < ActiveRecord::Migration[6.0]
   class CostLog < ApplicationRecord
     def up
       CostLog.all.each do |log|
-        log.risk_cost = log.calculate_risk_cost
+        log.calculate_risk_cost
         log.save!
       end
     end
