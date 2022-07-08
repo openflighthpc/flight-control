6cbe936bf1c51fd6876ffede59d96d471e68adf7
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2022-07-08 14:49:14 +0100

    Automatically remove edit icon when events stop being editable

--- app/javascript/packs/editable_event_check.js
+++ app/javascript/packs/editable_event_check.js
@@ -1,28 +1,35 @@
-// start checking events at the start of the next minute
 window.addEventListener('DOMContentLoaded', (event) => {
-    if (editableRows().length !== 0) {
+    if (editableRows().length > 0) {
+        // start checking events at the start of the next minute
         let secs = new Date().getSeconds();
-        setTimeout(editableCheck, 1000 );
-        // setTimeout(editableCheck, (60-secs)*1000 );
+        setTimeout(editableCheck, (60-secs)*1000 );
     }
 });
 
 function editableCheck() {
-    editableRows().each(function() {
-        let uneditableTime = Number($(this).data('uneditable'));
-        // check valid date
-        if (new Date(uneditableTime) <= new Date()) {
-            // event no longer editable
-            $(this).removeClass('editable-event-row');
-            // remove edit button
+    // remove edit icons for events that have become uneditable
+    editableRows().forEach(function(event) {
+        let uneditableTime = new Date( Number( event.dataset.uneditable ) );
+        validateTime(uneditableTime)
+        if (uneditableTime <= new Date()) {
+            event.classList.remove('editable-event-row');
+            let icon = event.querySelector('.fa-pencil')
+            icon.style.display = 'none'
         }
     })
-    if (editableRows().length !== 0) {
-        console.log('timeout')
-        // setTimeout(editableCheck,60000);
+
+    // check events again each minute
+    if (editableRows().length > 0) {
+        setTimeout(editableCheck,60000);
     }
 }
 
 function editableRows() {
-    return $('.editable-event-row');
+    return document.querySelectorAll('.editable-event-row');
+}
+
+function validateTime(uneditableTime) {
+    if (isNaN(uneditableTime)) {
+        throw 'Invalid time given for when event is no longer editable.';
+    }
 }
