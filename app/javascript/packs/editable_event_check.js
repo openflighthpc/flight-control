550ed54a94f62b4d73c5884c7e8c04f73a653524
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2022-07-08 15:02:49 +0100

    Improve validation for time that events become uneditable

--- app/javascript/packs/editable_event_check.js
+++ app/javascript/packs/editable_event_check.js
@@ -9,15 +9,13 @@ window.addEventListener('DOMContentLoaded', (event) => {
 function editableCheck() {
     // remove edit icons for events that have become uneditable
     editableRows().forEach(function(event) {
-        let uneditableTime = new Date( Number( event.dataset.uneditable ) );
-        validateTime(uneditableTime)
+        let uneditableTime = validateTime(Number(event.dataset.uneditable));
         if (uneditableTime <= new Date()) {
             event.classList.remove('editable-event-row');
             let icon = event.querySelector('.fa-pencil')
             icon.style.display = 'none'
         }
     })
-
     // check events again each minute
     if (editableRows().length > 0) {
         setTimeout(editableCheck,60000);
@@ -29,7 +27,9 @@ function editableRows() {
 }
 
 function validateTime(uneditableTime) {
-    if (isNaN(uneditableTime)) {
+    let dateTime = new Date(uneditableTime);
+    if (uneditableTime === 0 || uneditableTime == null || isNaN(dateTime)) {
         throw 'Invalid time given for when event is no longer editable.';
     }
+    return dateTime
 }
