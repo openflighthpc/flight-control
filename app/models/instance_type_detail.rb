75a72ac58bca6d1a55b952e834f46cbc430bdbd1
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2022-07-12 12:49:24 +0100

    Add instance type details model

---
+++ app/models/instance_type_detail.rb
@@ -0,0 +1,2 @@
+class InstanceTypeDetail < ApplicationRecord
+end
---
+++ db/migrate/20220712105329_create_instance_type_details.rb
@@ -0,0 +1,17 @@
+class CreateInstanceTypeDetails < ActiveRecord::Migration[6.0]
+  def change
+    create_table :instance_type_details do |t|
+      t.string  :instance_type
+      t.string  :region
+      t.decimal :price_per_hour
+      t.integer :cpu
+      t.integer :gpu
+      t.decimal :mem
+      t.string  :currency
+
+      t.timestamps
+    end
+
+    add_index :instance_type_details, [:instance_type, :region]
+  end
+end
