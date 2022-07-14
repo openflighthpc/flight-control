efe14f69d61a946dd31cfb5f0407537e01962651
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2022-07-14 15:23:24 +0100

    Allow numeric values of -1 to indicate invalid or missing data

--- app/models/instance_type_detail.rb
+++ app/models/instance_type_detail.rb
@@ -1,21 +1,19 @@
 class InstanceTypeDetail < ApplicationRecord
   validates :region, :price_per_hour, :cpu, :gpu, :mem, :currency, presence: true
-  validates :price_per_hour, :cpu, :gpu, :mem, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :price_per_hour, :cpu, :gpu, :mem, :numericality => { :greater_than_or_equal_to => -1 }
   validates :instance_type,
             presence: true,
             uniqueness: { scope: :region, message: -> (object, _) { object.repeated_instance_type_error } }
 
   def record_details
-    valid = valid?
     old_details = repeated_instance_type
     if old_details
       old_details.update_details(self)
     else
       raise 'No valid data to record' if valid_attributes.empty?
-      set_default_values unless valid
+      set_default_values
       save!
     end
-    # alert somehow about any ignored invalid attributes
   end
 
   def repeated_instance_type
@@ -37,10 +35,12 @@ class InstanceTypeDetail < ApplicationRecord
 
   def invalid_attributes
     return @invalid_attributes if defined?(@invalid_attributes)
+    valid?
     errors = self.errors.messages
     @invalid_attributes = []
-    attributes.keys.each do |attr|
-      if errors.key?(attr.to_sym)
+    attributes.each do |attr, value|
+      invalid_number = value.is_a?(Numeric) ? (value < 0) : false
+      if errors.key?(attr.to_sym) || invalid_number
         @invalid_attributes.append(attr) unless errors[attr.to_sym].first == repeated_instance_type_error
       end
     end
@@ -51,9 +51,9 @@ class InstanceTypeDetail < ApplicationRecord
     return if invalid_attributes.empty?
     invalid_attributes.each do |attr|
       if self.class.columns_hash[attr].type == :string
-        assign_attributes( { attr => 'invalid' })
+        assign_attributes( { attr => 'INVALID' })
       else
-        assign_attributes( { attr => 0 } )
+        assign_attributes( { attr => -1 } )
       end
     end
   end
