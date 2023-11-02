0ba480d05815e1188deaa777597d0eae1bf67485
Matt Ruddy <matt@calcwise.com>
2023-11-02 18:10:11 +0000

    Make azure_instance_manager use API

--- app/services/azure_instance_manager.rb
+++ app/services/azure_instance_manager.rb
@@ -1,46 +1,22 @@
 class AzureInstanceManager < AzureService
 
   def update_instance_statuses(action, resource_group, node_names, verbose=false)
-    command = action.to_s == "on" ? "start" : "deallocate"
-    node_names.each do |node|
-      uri = "https://management.azure.com/subscriptions/#{@project.subscription_id}/resourceGroups/#{resource_group}/providers/Microsoft.Compute/virtualMachines/#{node}/#{command}"
-      query = {
-        'api-version': '2020-12-01',
-      }
-      attempt = 0
-      error = AzureApiError.new("Timeout error querying compute nodes for project"\
-                                "#{@project.name}. All #{MAX_API_ATTEMPTS} attempts timed out.")
-      begin
-        @project.authoriser.refresh_auth_token
-        attempt += 1
-        response = HTTParty.post(
-          uri,
-          query: query,
-          headers: {"Authorization": "Bearer #{@project.bearer_token}",
-                    "Accept" => "application/json"
-                   },
-          timeout: DEFAULT_TIMEOUT
-        )
-        if response.success?
-          # do nothing - the API doesn't return anything
-        elsif response.code == 504
-          raise Net::ReadTimeout
-        else
-          raise AzureApiError.new("Error querying compute nodes for project #{@project.name}."\
-                                  "\nError code #{response.code}.\n#{response if @verbose}")
-        end
-      rescue Net::ReadTimeout
-        msg = "Attempt #{attempt}: Request timed out.\n"
-        if response
-          msg << "Error code #{response.code}.\n#{response if @verbose}\n"
-        end
-        error.error_messages.append(msg)
-        if attempt < MAX_API_ATTEMPTS
-          retry
-        else
-          raise error
-        end
-      end 
+    node_names.each do |node_name|
+      command = action == "on" ? "start-instance" : "stop-instance"
+      response = http_request(uri: 'http://0.0.0.0:4567/providers/azure/#{command}',
+                              headers: {"Project-Credentials" => {"resource_group": resource_group}.inspect},
+                              body: { "node_name" => node_name }.to_json
+                             )
+      case response.code
+      when 200
+        #Instance state set successfully
+      when 401
+        raise 'Credentials missing or incorrect'
+      when 404
+        raise 'Provider #{creds["provider"]} and/or instance #{instance_id} not found'
+      when 500
+        raise 'Internal error in Control API'
+      end
     end
   end
 end
