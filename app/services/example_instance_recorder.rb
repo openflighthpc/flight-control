cace0120ba99f3de48e2d5865751da940fd2912a
Matt Ruddy <matt@calcwise.com>
2023-12-05 15:32:49 +0000

    Add missing bracket

--- app/services/example_instance_recorder.rb
+++ app/services/example_instance_recorder.rb
@@ -60,7 +60,7 @@ class ExampleInstanceRecorder
 
   def validate_credentials
     response = http_request(uri: 'http://0.0.0.0:4567/providers/example-provider/validate-credentials',
-                            headers: {'Project-Credentials' => {'PROJECT_NAME': @project.project_name}.inspect
+                            headers: {'Project-Credentials' => {'PROJECT_NAME': @project.project_name}.inspect}
                            )
     response.code==200
   end
