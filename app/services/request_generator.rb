9daa8491999b129f1266937ce94d2847fea999b1
Matt Ruddy <matt@calcwise.com>
2023-12-06 14:33:56 +0000

    Fix query terms not being included

--- app/services/request_generator.rb
+++ app/services/request_generator.rb
@@ -7,11 +7,11 @@ def http_request(uri:, request_type: "get", headers: {}, query: {}, body: nil )
   uri = URI(uri)
   http = Net::HTTP.new(uri.host, uri.port)
 
+  uri.query = URI.encode_www_form(query)
   request = request_type=="get" ? Net::HTTP::Get.new(uri.request_uri) : Net::HTTP::Post.new(uri.request_uri)
   headers.each do |key, value|
     request[key] = value
   end
-  request.set_form_data(query)
   request.body = body
 
   http.request(request)
