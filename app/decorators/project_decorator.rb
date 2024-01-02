502d46e088b6ee8e905f74184d5316242a4884a6
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2024-01-02 17:05:26 +0000

    Add estimated costs to tooltips where relevant on billing chart

--- app/decorators/project_decorator.rb
+++ app/decorators/project_decorator.rb
@@ -19,14 +19,13 @@ class ProjectDecorator < Draper::Decorator
 
   def billing_history_tooltips
     billing_history.map do |cycle|
-      [
-        "#{pretty_date(cycle[:start])} - #{pretty_date(cycle[:end])}",
-        "",
-        "Total cost: #{cycle[:cost].to_i}cu",
-        "",
-        "Click to view costs breakdown",
-        "for this billing cycle",
-      ]
+      tooltip = ["#{pretty_date(cycle[:start])} - #{pretty_date(cycle[:end])}", ""]
+      if cycle[:current]
+        tooltip << "Cost so far: #{cycle[:costs_so_far]}cu" << ""
+      end
+      tooltip << "#{cycle[:estimate] ? "Estimated t" : "T"}otal cost: #{cycle[:cost].to_i}cu" << ""
+      tooltip << "Click to view costs breakdown" << "for this billing cycle"
+      tooltip
     end
   end
 
