cfe3300329b646d9f5d4a37920aef38269c15dbe
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2024-01-03 18:00:20 +0000

    Refactor to make decorator methods private

--- app/decorators/project_decorator.rb
+++ app/decorators/project_decorator.rb
@@ -1,17 +1,9 @@
 class ProjectDecorator < Draper::Decorator
   delegate_all
 
-  def billing_history
-    @billing_history ||= costs_plotter.historic_cycle_details
-  end
-
   def billing_history_years
     billing_history.map { |cycle| billing_date(cycle).year }
                    .uniq
-  end
-
-  def billing_history_by_year(year)
-    billing_history.select { |cycle| billing_date(cycle).year == year }
   end
 
   def billing_chart_data
@@ -25,20 +17,8 @@ class ProjectDecorator < Draper::Decorator
 
   def billing_chart_tooltips
     billing_history_years.map do |year|
-      [ year, billing_history_tooltips(year) ]
+      [ year, billing_chart_tooltips_by_year(year) ]
     end.to_h
-  end
-
-  def billing_history_tooltips(year)
-    billing_history_by_year(year).map do |cycle|
-      tooltip = ["#{pretty_date(cycle[:start])} - #{pretty_date(cycle[:end])}", ""]
-      if cycle[:current]
-        tooltip << "Cost so far: #{cycle[:costs_so_far]}cu" << ""
-      end
-      tooltip << "#{cycle[:estimate] ? "Estimated t" : "T"}otal cost: #{cycle[:cost].to_i}cu" << ""
-      tooltip << "Click to view costs breakdown" << "for this billing cycle"
-      tooltip
-    end
   end
 
   private
@@ -49,5 +29,25 @@ class ProjectDecorator < Draper::Decorator
 
   def billing_date(cycle)
     cycle[:end] + 1.day
+  end
+
+  def billing_history
+    @billing_history ||= costs_plotter.historic_cycle_details
+  end
+
+  def billing_history_by_year(year)
+    billing_history.select { |cycle| billing_date(cycle).year == year }
+  end
+
+  def billing_chart_tooltips_by_year(year)
+    billing_history_by_year(year).map do |cycle|
+      tooltip = ["#{pretty_date(cycle[:start])} - #{pretty_date(cycle[:end])}", ""]
+      if cycle[:current]
+        tooltip << "Cost so far: #{cycle[:costs_so_far]}cu" << ""
+      end
+      tooltip << "#{cycle[:estimate] ? "Estimated t" : "T"}otal cost: #{cycle[:cost].to_i}cu" << ""
+      tooltip << "Click to view costs breakdown" << "for this billing cycle"
+      tooltip
+    end
   end
 end
